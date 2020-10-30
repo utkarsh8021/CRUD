d8ce5c0ceeef76a855aa246dabd4e67133e67542
utkarsh8021 <mishra8021@gmail.com>
2020-10-30 17:01:21 +0530

    edit

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,7 @@
 let express = require('express');
 let router = express.Router();
 let mongoose = require('mongoose');
+var ObjectID = require('mongodb').ObjectID;
 
 // define the game model
 let Book = require('../models/books');
@@ -38,10 +39,24 @@ Book.findById(req.params.id, function(err, foundBook){
   if(err){
       res.redirect("/");
   }else{
-    res.render("books/details", {title:'Edit Book Details', books: foundBook});
+    res.render("books/edit", {title:'Edit Book Details', books: foundBook});
   }
 })
 })
+
+router.post('/edit/:id', function(req, res){
+  let data = req.body;
+  let id = new ObjectID(req.params.id);
+  data._id = id;
+  Book.findByIdAndUpdate(id, data).then(function(err) {
+    console.log(err, 'updated');
+    Book.findOne().then(function(items) {
+      console.log(items)
+      books = items;
+      res.redirect('/books');
+    });
+  });
+});
 
 router.get('/delete/:id', function(req, res, next) {
   Book.findByIdAndRemove({_id:req.params.id}, function(err){
